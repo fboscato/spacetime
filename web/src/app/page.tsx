8356e4f1a6a59da8ef6a3984f137cdca6562d1cf
fernandoboscato <fernandoboscato@faee.com.br>
2023-05-19 16:16:53 -0300

    aula 02

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,22 @@
 export default function Home() {
   return (
-    <div className="m-10 h-20 w-20 rounded bg-red-500 text-2xl font-bold uppercase">
-      <h1>Hello Wold</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+        <div className="absolute right-0 top-1/2 h-[288px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
+      </div>
+
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
