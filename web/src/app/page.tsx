67d659ffa36c489f96d788be22b15d5c2e1087ce
fernandoboscato <fernandoboscato@faee.com.br>
2023-05-25 16:55:36 -0300

    aula 02

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,16 @@
 import { User } from 'lucide-react'
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+import Image from 'next/image'
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2 ">
+      {/* Left */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+        {/* Blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+        {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2  bg-stripes"></div>
+        {/* Sign In */}
         <a
           href=""
           className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
@@ -17,8 +23,12 @@ export default function Home() {
             memórias!
           </p>
         </a>
+        {/* Hero */}
+        <div>
+          <Image src={nlwLogo} alt="NLW Space" />
+        </div>
       </div>
-
+      {/* Right */}
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
         <div className="flex flex-1 items-center justify-center">
           <p className="w-[360px] text-center leading-relaxed">
